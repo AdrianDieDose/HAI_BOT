728e43c24ce52730614c99d995a5d19ae71c3ae7
AdrianDieDose <AdrianDuda2019@gmail.com>
2021-05-27 16:17:11 +0200

    Adding poll structure

--- commands/poll.js
+++ commands/poll.js
@@ -2,8 +2,18 @@ module.exports = {
     name: 'poll',
     description: 'Makes a poll',
     execute(message, client, args) {
-        message.channel.send('-poll | Starts poll');
-        
+        message.channel.send('-poll start [topic] [option1] [option2] [option3] | Start poll with the topic and options');
+        message.channel.send('-poll vote [1/2/3] | Vote on poll');
+        message.channel.send('-poll end | End poll');
+        message.channel.send('test: '+ message + client + args);
+        let pollActive = true;
+        //while(pollActive){
+
+
+
+
+
+        //}
     }
 
 }
