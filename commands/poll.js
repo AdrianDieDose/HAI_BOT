c8282cd5e7cf84b262219093e7ef930bf4bf4948
AdrianDieDose <AdrianDuda2019@gmail.com>
2021-05-28 21:39:06 +0200

    Fixed variables saving with saving them in the object itself, added status, programm resets, and almost completet voting.

--- commands/poll.js
+++ commands/poll.js
@@ -1,41 +1,77 @@
 module.exports = {
-    name: 'poll',
-    description: 'Makes a poll',
-    execute(message, client, args) {
-        let pollActive = true;
-        if(pollActive){
-            if(args[0] == "start" && args.length > 1 ){
-                const topic = args[1];
-                const options = [];
-                message.channel.send("Poll topic: " + topic);
-                    for(let i = 2;i < args.length;i++){
-                        message.channel.send("Option " + (i-1) + ": " + args[i]);
-                        options.push([args[i], 0]);
-                    }
-                console.log(options);
-            } else if(args[0] == "vote"){
-
-            } else if(args[0] == "end"){
-
-            } else if(args[0] == "status"){
-
-            } else if(args[0] == "help"){
-                message.channel.send('-poll help | Prints all help commands');
-                message.channel.send('-poll start [**topic**] | [**option1**] [**option2**] [**option3...**] | Start poll with the topic and options');
-                message.channel.send('-poll vote [**1/2/3...**] | Vote on poll');
-                message.channel.send('-poll status | Shows polls status');
-                message.channel.send('-poll end | End poll');
-            } else {
-                pollActive = false;
-                options = [];
-                message.channel.send('-poll help | Prints all help commands');
-            }
-
-
-
-
+  name: "poll",
+  description: "Makes a poll",
+  topicChoosen: false,
+  optionsChoosen: false,
+  topic: [],
+  options: [],
+  pollOwnersId: "",
+  pollOwnersName: "",
+  pollVotersIds: [],
+  execute(message, client, args) {
+    // Add user validation ?
+    if (args[0] == "topic" && args.length > 1) {
+      this.pollOwnersId = message.author.id;
+      this.pollOwnersName = message.author.username;
+      this.topic = args.slice(1, args.length);
+      this.topicChoosen = true;
+      message.channel.send("Poll owner: **" + this.pollOwnersName + "**");
+      message.channel.send("Poll topic: **" + this.topic.join(" ") + "**");
+    } else if (args[0] == "options" && message.author.id == this.pollOwnersId) {
+      if (this.topicChoosen) {
+        this.options = [];
+        for (let i = 1; i < args.length; i++) {
+          message.channel.send("Option " + i + ": " + args[i]);
+          this.options.push([args[i], 0]);
+          this.optionsChoosen = true;
         }
-        
+      } else {
+        message.channel.send(
+          "You need to specify a **Topic** first! | Type **-poll help** to find out more."
+        );
+      }
+    } else if (args[0] == "vote" && this.optionsChoosen == true) {
+      let i = 0;
+      do {
+        if (message.author.id == this.pollVotersIds[i]) {
+          message.channel.send(
+            "You've already voted, **" + this.pollOwnersName + "**!"
+          );
+          return;
+        } else {
+          i++;
+        }
+      } while (i < this.pollVotersIds);
+      this.pollVotersIds.push(message.author.id);
+      console.log(message.author.id);
+    } else if (args[0] == "end" && message.author.id == this.pollOwnersId) {
+    } else if (args[0] == "status") {
+      message.channel.send("Poll owner: **" + this.pollOwnersName + "**");
+      message.channel.send("Poll topic: **" + this.topic.join(" ") + "**");
+      for (let i = 0; i < this.options.length; i++) {
+        message.channel.send("Option " + (i + 1) + ": " + this.options[i][0]);
+      }
+    } else if (args[0] == "help") {
+      message.channel.send("-poll help | Prints all help commands.");
+      message.channel.send("-poll topic [**topic**] | Specifies poll topic.");
+      message.channel.send(
+        "-poll options [**option1**], [**option2**] ... | Specifies poll options."
+      );
+      message.channel.send("-poll vote [**1/2/3...**] | Vote on poll.");
+      message.channel.send("-poll status | Shows polls status.");
+      message.channel.send("-poll end | End poll.");
+    } else {
+      //Needs a reset fix... if no argument is casted everything is reset... what we dont want of course
+      //console.log("test rip");
+      console.log("Did it return ?");
+      this.optionsChoosen = false;
+      this.topicChoosen = false;
+      this.topic = [];
+      this.options = [];
+      this.pollOwnersId = "";
+      this.pollVotersIds = [];
+      this.pollOwnersName = "";
+      this.message.channel.send("-poll help | Prints all help commands.");
     }
-
-}
+  },
+};
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
 const guild = new Discord.Guild();
 
-const prefix = '-';
+const prefix = "-";
 
-const fs = require('fs');
+const fs = require("fs");
 
 const secretClientId = require("../ID/clientid.js");
 
@@ -14,69 +14,69 @@ const memberCount = require("./member-count");
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith("js"));
 
 /*Join message*/
-client.on('guildMemberAdd', member =>{
-    const welcomeChannel = client.channels.cache.find(channel => channel.id === "821715172451680328");
-    welcomeChannel.send('Brinki lässt: **' + member.user.username + '** grüßen. '+ "**" + member.guild.name + "**!");
-
+client.on("guildMemberAdd", (member) => {
+  const welcomeChannel = client.channels.cache.find(
+    (channel) => channel.id === "821715172451680328"
+  );
+  welcomeChannel.send(
+    "Brinki lässt: **" +
+      member.user.username +
+      "** grüßen. " +
+      "**" +
+      member.guild.name +
+      "**!"
+  );
 });
 
-client.once('ready', () => {
-    console.log('HAI_BOT is online uwu!');
-    client.user.setActivity("Slapping Brinkhoffs ass 👋");
-    memberCount(client);
-   // console.log(client.user(529619290886766592));
+client.once("ready", () => {
+  console.log("HAI_BOT is online uwu!");
+  client.user.setActivity("Slapping Brinkhoffs ass 👋");
+  memberCount(client);
+  // console.log(client.user(529619290886766592));
 });
 
 // Loops thru files to find commands.
-for(const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
 // Checks chat.
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    // Add commands to look for in chat. Add new commands in the commands folder.
-    switch (command) {
-        case 'ping':
-            client.commands.get('ping').execute(message, args);
-        break;
-        case 'spam':
-            client.commands.get('spam').execute(message, args);
-        break;
-        case 'help':
-            client.commands.get('help').execute(message, args);
-        break;
-        case 'wichteln':
-            client.commands.get('wichteln').execute(client, message, args);
-        break;
-        case 'count':
-            client.commands.get('count').execute(message, client);
-        break;
-        case 'poll':
-            client.commands.get('poll').execute(message, client, args);
-        break;
-     //   case 'summonfriends':
-     //       
-     //   break;
-    }
+  // Add commands to look for in chat. Add new commands in the commands folder.
+  switch (command) {
+    case "ping":
+      client.commands.get("ping").execute(message, args);
+      break;
+    case "spam":
+      client.commands.get("spam").execute(message, args);
+      break;
+    case "help":
+      client.commands.get("help").execute(message, args);
+      break;
+    case "wichteln":
+      client.commands.get("wichteln").execute(client, message, args);
+      break;
+    case "count":
+      client.commands.get("count").execute(message, client);
+      break;
+    case "poll":
+      client.commands.get("poll").execute(message, client, args);
+      break;
+    //   case 'summonfriends':
+    //
+    //   break;
+  }
 });
-
-
-
-
-
-
-
-
-
-
 
 client.login(secretClientId.clientId);
